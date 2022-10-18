5f151ac5086fb40bdeff4a19618b995a9be4cd8f
celine-schmid <celine.schmid@stud.hslu.ch>
2022-10-18 20:13:40 +0200

    bootstrap, animation fail
    Co-authored-by: annakleger <annakleger@users.noreply.github.com>

---
+++ assets/javascript/script.js
@@ -0,0 +1,16 @@
+// function setup() {
+//     createCanvas(windowWidth, windowHeight);
+//     frameRate(2);
+
+
+// }
+
+// function draw() {
+
+//     background('#131423');
+//     fill(0);
+//     ellipse(random(0, width), random(0, height), 200, 200);
+//     textAlign(CENTER, CENTER);
+//     fill('#84416e')
+//     text('Wie oft berührst du dein Gesicht?')
+// }
